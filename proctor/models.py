72231926fdbc3e147d75d7a68c0e4509928fdb9c
Diptanil Saha <diptanil.dev@gmail.com>
2024-01-11 00:35:37 +0530

    fix: typo on models.py

--- proctor/models.py
+++ proctor/models.py
@@ -13,7 +13,7 @@ class User(UserMixin, db.Model):
     password_hash: Mapped[str] = mapped_column(db.String(128))
 
     def __repr__(self):
-        return '<User %r' % self.id
+        return '<User %r>' % self.id
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
