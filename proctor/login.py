d5f2083b0e85c752bb7636a2df5d5c1970c6adf8
Diptanil Saha <diptanil.dev@gmail.com>
2024-01-11 01:42:30 +0530

    refactor: login_manager with SQLAlchemy 2.0

--- proctor/login.py
+++ proctor/login.py
@@ -1,6 +1,6 @@
-from proctor import login_manager
+from proctor import login_manager, db
 from proctor.models import User
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
